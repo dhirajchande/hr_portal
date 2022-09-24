41f50943a260cdc958d468334c2775471fc8d47f
dhirajchande <dhirajchande@gmail.com>
2022-09-24 22:26:41 +0530

    sidebar added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeesComponent } from './employees/employees.component';
+import { JobsComponent } from './jobs/jobs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"employees", component:EmployeesComponent},
+  {path:"jobs", component:JobsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
