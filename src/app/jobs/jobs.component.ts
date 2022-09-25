7f38c112c247669600d1d4ee23cb6adaebcc4c88
dhirajchande <dhirajchande@gmail.com>
2022-09-25 18:57:13 +0530

    job component modified

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
+
 
 @Component({
   selector: 'app-jobs',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jobs.component.scss']
 })
 export class JobsComponent implements OnInit {
+  joblist: any;
+  dtOptions: DataTables.Settings = {
+    pagingType: 'full_numbers'
+  };
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit(): void {
+
+    this.httpClient.get("https://demo.club360.page/api/jobs").subscribe( (data: any) => {
+	console.log(data);
+	this.joblist = data;
+})
+
   }
 
 }
